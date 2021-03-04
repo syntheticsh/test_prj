2040f2d75c8cc6f6cbf12d0614d4ac3a98443679
syntheticsh <syntheticsh@intersvyaz.net>
2021-03-04 17:07:05 +0500

    test

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class'             => 'yii\db\Connection',
+    'dsn'               => 'pgsql:host=database;port=5432;dbname=testprj',
+    'username'          => 'yii',
+    'password'          => '123456',
+    'charset'           => 'utf8',
+    'enableSchemaCache' => false,
+];
---
+++ config/web.php
@@ -0,0 +1,63 @@
+<?php
+
+$config = [
+    'id'         => 'basic',
+    'basePath'   => dirname(__DIR__),
+    'bootstrap'  => ['log'],
+    'aliases'    => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request'      => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '0SXJMIA8HMCcGCphieoZr5Uu7MAYqMvd',
+        ],
+        'cache'        => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user'         => [
+            'identityClass'   => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'log'          => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets'    => [
+                [
+                    'class'  => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db'           => require __DIR__ . '/db.php',
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params'     => require __DIR__ . '/params.php',
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class'      => 'yii\debug\Module',
+        'allowedIPs' => ['*'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class'      => 'yii\gii\Module',
+        'allowedIPs' => ['*'],
+    ];
+}
+
+return $config;
